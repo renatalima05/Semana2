9763fc9a080347001631194eae900e2ff247630e
Renata Lima <renatacglima05@gmail.com>
2021-05-31 16:18:19 -0300

    resolução da atividade1

---
+++ atividade1.js
@@ -0,0 +1,7 @@
+//1 - Solicite o preço de uma mercadoria e o percentual de desconto 20%. Exiba no console o valor do desconto e o preço a pagar.
+
+let mercadoria = 275;
+const desconto = mercadoria * 0.2; 
+const total = mercadoria - desconto;
+
+console.log(`desconto de ${desconto}, total ${total}`)
