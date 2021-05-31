622650fb56d26fd1902d1624d3bb2e4ad1295a11
Renata Lima <renatacglima05@gmail.com>
2021-05-31 18:46:25 -0300

    resolução da atividade10

---
+++ atividade10.js
@@ -0,0 +1,13 @@
+//Crie uma variavel que recebe o ano de nascimento da pessoa informando se ela é maior de idade ou menor.
+
+let anoNascimento = 2001
+
+if (anoNascimento <= 2003) {
+    console.log(`é maior de idade`)
+
+}
+
+else {
+    console.log(`não é maior de idade`)
+
+}
