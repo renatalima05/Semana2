f6c5c7c2331ffd611b14e2057aeba2dbdf0debe4
Renata Lima <renatacglima05@gmail.com>
2021-05-31 18:21:08 -0300

    resolução da atividade7

---
+++ atividade7.js
@@ -0,0 +1,16 @@
+// Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, 
+//se os números forem iguais, mostre uma mensagem no console "Os números são iguais"
+
+let n1 = 2, n2 = 3;
+if (n1 > n2) {
+     
+    console.log(`${n1} é maior que ${n2}`)
+
+}
+
+else {
+
+    console.log(`${n2} é maior que ${n1}`)
+
+
+}
