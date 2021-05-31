34e9dd40c950343a82b4fb62116822045c650415
Renata Lima <renatacglima05@gmail.com>
2021-05-31 18:06:03 -0300

    resolução da atividade6

---
+++ atividade6.js
@@ -0,0 +1,43 @@
+//Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente.
+let numero = 7
+switch (numero) {
+
+    
+
+    case 1: 
+        console.log(`domingo`);
+        break;
+       
+
+    case 2:
+        console.log(`segunda`);
+        break;
+
+    case 3:
+        console.log(`terça`);
+        break;
+
+    case 4:
+        console.log(`quarta`);
+        break;
+
+    case 5:
+        console.log(`quinta`);
+        break;
+
+    case 6:
+        console.log(`sexta`);
+        break;
+
+    case 7:
+        console.log(`sabado`);
+        break;  
+         
+    default: 
+        console.log(`o numero não é um dia da semana`)
+
+}
+
+
+
+
