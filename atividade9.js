7a81d0fbcc63997c37e8fed6c2bc9caa196c97c1
Renata Lima <renatacglima05@gmail.com>
2021-05-31 18:39:45 -0300

    resolução da atividade9

---
+++ atividade9.js
@@ -0,0 +1,13 @@
+//Crie uma variavel que retorna a palavra (impar/par) de acordo com seu parâmetro.
+
+let numero = 4
+ 
+if (numero%2 == 0) {
+    console.log(`${numero} é par`)
+
+}
+
+else {
+    console.log(`${numero} é impar`)
+
+}
