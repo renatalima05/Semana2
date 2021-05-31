a867167a3a5f249db6595ac83c8a28d1b8378a12
Renata Lima <renatacglima05@gmail.com>
2021-05-31 16:37:12 -0300

    resolução da atividade3

---
+++ atividade3.js
@@ -0,0 +1,8 @@
+// Crie um algoritmo que leia o valor de um jantar, 
+//calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
+
+let jantar = 85;
+const taxa = jantar * 0.1;
+let total = jantar + taxa;
+
+console.log(`O valor da taxa do garçom é ${taxa}, o total a ser pago é ${total}`);
